82f580dd26789349c72cf51b649622dee62b44c8
Furkan Kiziltan <kiziltan.furkan@gmail.com>
2021-11-17 18:53:39 +0100

    Express Backend set up, views are re-written using Pug

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
 const DBOperator = require("./DBOperator");
 
-async function main(){
-    const dbOperator = new DBOperator();
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+const dbOperator = new DBOperator();
 
+/**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+/**
+ * Routes Definitions
+ */
+app.get("/", async (req, res) => {
     await dbOperator.connectToDB();
     const allRecipes = await dbOperator.getAllRecipies();
-    //await dbOperator.addANewRecipe({title: "Dummy"});
-    await dbOperator.closeDBConnection();
-}
+    res.render("homepageLayout", { recipes: allRecipes });
+    res.status(200);  
+});
 
-main();
+app.post("/addRecipe", async (req, res) => {
+    //console.log(req.body);
+    await dbOperator.addANewRecipe(req.body);
+    res.redirect("/")
+});
+
+app.get("/addRecipe", (req, res) => {
+    res.render("addRecipeLayout");
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
